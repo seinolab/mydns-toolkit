f1cbc9ec7da115cd39ff6dd20bb88160c878fa7c
Takahiro Seino <5173733+seinolab@users.noreply.github.com>
2021-12-26 22:39:19 +0900

    urlencode supported.

--- delete.sh
+++ delete.sh
@@ -5,8 +5,8 @@ AUTH=`head -c -1 /root/.mydns | base64`
 
   env \
 | grep ^CERTBOT \
-| sed -e 's/$/\&/' -e '$ a EDIT_CMD=DELETE' \
-| curl -X POST -d @- -H "Authorization: Basic ${AUTH}" -s -i ${URL} \
+| sed -e 's/$/"/' -e 's/^/-d "/' -e '$ a -d EDIT_CMD=DELETE' \
+| xargs curl -X POST -d @- -H "Authorization: Basic ${AUTH}" -s -i ${URL} \
 | head -1 \
 | sed -e "s/^/DELETE ${CERTBOT_DOMAIN} - /" \
 | logger -t MyDNS
--- regist.sh
+++ regist.sh
@@ -5,8 +5,8 @@ AUTH=`head -c -1 /root/.mydns | base64`
 
   env \
 | grep ^CERTBOT \
-| sed -e 's/$/\&/' -e '$ a EDIT_CMD=REGIST' \
-| curl -X POST -d @- -H "Authorization: Basic ${AUTH}" -s -i ${URL} \
+| sed -e 's/$/"/' -e 's/^/-d "/' -e '$ a -d EDIT_CMD=REGIST' \
+| xargs curl -X POST -d @- -H "Authorization: Basic ${AUTH}" -s -i ${URL} \
 | head -1 \
 | sed -e "s/^/REGIST ${CERTBOT_DOMAIN} - /" \
 | logger -t MyDNS
